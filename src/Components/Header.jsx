d99efe37f545346f801af5e1331a2232e0b00988
eslachance <eslachance@gmail.com>
2023-04-28 07:52:42 -0400

    1 binaries uploaded

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,8 @@
 import { A } from '@solidjs/router';
 import logo from '@/PowerKnobsNameWhite.svg';
 
-export const Header = () => {
-  return (
-    <div class="fixed top-0 w-full flex justify-center text-white h-[75px] pl-[50px] bg-purple font-sans">
-      <div class="w-[1280px] max-w-[1280px] flex items-center h-full">
-        <A href="/">
+/*
+<A href="/">
           <img class="h-[75px] w-50 mr-5" src={logo} alt="PowerKnobs" />
         </A>
         <A href="/" class="mx-2 font-sans no-underline">
@@ -14,6 +11,13 @@ export const Header = () => {
         <A href="/products" class="mx-2 font-sans no-underline">
           Products
         </A>
+*/
+
+export const Header = () => {
+  return (
+    <div class="fixed top-0 w-full flex justify-center text-white h-[75px] bg-purple font-sans">
+      <div class="w-[1280px] max-w-[1280px] flex justify-center h-full">
+        <div class="w-[256px] h-[256px] border-1 border-solid"></div>
       </div>
     </div>
   );
